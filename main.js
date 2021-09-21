e87cf89a18a4307c704a0b9fb8fc3a69776aa8d3
Rp317 <89706794+Rp317@users.noreply.github.com>
2021-09-20 20:11:08 -0500

    Update main.js
    pls helps

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 // set this up 
-const lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-const higherLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-	"N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
-const symbols = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", ":", ";", ",", "<", ">", ".", "/", "?"];
-const split = [];
+const uppercase = new RegExp('(?=.*?[A-Z])');
+const lowercase = new RegExp("(?=.*?[a-z])");
+const number = new RegExp('(?=.*?[0-9])')
+const symbol = new RegExp('(?=.*?[#?!@$%^&*-+=|~:;?/.,<>])')
 const prompt = require("prompt-sync")({ sigint: true });
 let username = "";
 let password = "";
-let prompt20 = []
 const narrator = "sam";
 let randomint = Math.floor(Math.random() * 10000 - 1000 + 1000);
 function sleep(ms) {
@@ -43,62 +40,60 @@ async function signUp() {
 	username = prompt1;
 	//password section!
 	console.log("\x1b[39m%s\x1b[0m", "*Must be more than 10 charecters!*");
-	console.log(
-		"\x1b[36m%s\x1b[0m",
-		"Please use at least one uppercase, lowercase, number and symbol charecters!"
-	);
+	console.log("\x1b[36m%s\x1b[0m","Please use at least one uppercase, lowercase, number and symbol charecters!")
 	//Really you could be reducing a lot of duplication with better use of objects and arrays, so you can loop (or even make use of functions like .every())
 	let prompt2 = prompt("Password?: ");
-	function step1() {
-	for(const element of prompt2) {
-		if(lowerLetters.includes(element)) {
-			console.log('pog')
-			step2()
-			break;
+	let done = true
+	let check1 = new Promise ((resolve, reject) => {
+		if (uppercase.test(prompt2)) {
+			const something1 = 'waiting...'
+  		resolve(something1)
 		} else {
-			console.log('no')
-			break;
+			const something1No = 'please add an uppercase letter!'
+			reject(something1No)
 		}
-	}}
-	function step2() {
-	for(const element of prompt2) {
-		if(higherLetters.includes(element)) {
-			console.log('pog')
-			step3()
-			break;
+	})
+	let check2 = new Promise ((resolve, reject) => {
+		if (lowercase.test(prompt2)) {
+			const something2 = 'waiting...'
+  		resolve(something2)
 		} else {
-			console.log('no')
-			break;
+			const something2no = 'please add an uppercase letter!'
+			reject(something2no)
 		}
-	}}
-	function step3() {
-	for(const element of prompt2) {
-		if(numbers.includes(element)) {
-			console.log('pog')
-			step4()
-			break;
+	})
+
+	let check3 = new Promise ((resolve, reject) => {
+		if (number.test(prompt2)) {
+			const something3 = 'waiting...'
+  		resolve(something3)
 		} else {
-			console.log('no')
-			break;
+			const something3no = 'please add an number!'
+			reject(something3no)
 		}
-	}}
-	function step4() {
-	for(const element of prompt2) {
-		if(symbols.includes(element)) {
-			console.log('pog')
-			break;
+	})
+		let check4 = new Promise ((resolve, reject) => {
+		if (symbol.test(prompt2)) {
+			const something4 = 'waiting...'
+  		resolve(something4)
 		} else {
-			console.log('no')
-			break;
+			const something4no = 'please add an symbol!'
+			reject(something4no)
 		}
-	}}
-	step1()
+	})
+	let ab = false
+	while(ab = false) {
+		Promise.all([check1, check2, check3, check4])
+		.then(function(data){console.log('done!')})
+		.then(ab = true)
+ 		.catch(function(err){console.error(err)});
+	}
 	while (isComplete < 2) {
 		if (prompt2.length < 10) {
 			console.log("please make your password longer!");
 			prompt2 = prompt("Password?: ");
 		} else {
-				isComplete = isComplete + 1;
+			isComplete = isComplete + 1;
 		}
 	}
 }
